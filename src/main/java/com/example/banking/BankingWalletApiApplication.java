ff64c9d0f4e89980919e2b676c190b0a58a43e0a
FlyVo <nhuphivo@gmail.com>
2026-06-22 10:46:44 +0700

    chore: initialize Spring Boot project

---
+++ src/main/java/com/example/banking/BankingWalletApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingWalletApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingWalletApiApplication.class, args);
+    }
+
+}
